f618c8c3a14d07d3333082f20167cadf021b71cb
Nikolay Bayborodin <nbbrdn@gmail.com>
2024-02-16 13:02:36 +1000

    Replate library to view360

--- app.js
+++ app.js
@@ -1,51 +1,24 @@
-fetch("data.json")
-        .then(response => response.json())
-        .then(data => {
-            var miniMap = document.getElementById("miniMap");
-            miniMap.src = data.plane;
+function openModal() {
+    console.log("here");
+    document.getElementById('modal-360').style.display = 'block';
 
-            var miniMapContainer = document.getElementById("miniMapContainer");
-            var miniMapWidth = miniMapContainer.offsetWidth;
-            var miniMapHeight = miniMapContainer.offsetHeight;
+    const viewer = new View360("#viewer", {
+      initialZoom: 0.5,
+      autoplay: {
+        delay: 5000,
+        delayOnMouseLeave: 1000,
+        pauseOnHover: true,
+        speed: 0.2,
+      },
+      projection: new View360.EquirectProjection({
+        src: "images/marker1.jpg",
+        video: false,
+      }),
+      
+    });
+}
 
-            // Load first view
-            if (data.markers.length > 0) {
-                var firstMarker = data.markers[0];
-                pannellum.viewer('panorama', {
-                    "type": "equirectangular",
-                    "panorama": firstMarker.file,
-                    "autoLoad": true,
-                    "showControls": false,
-                    "autoRotate": -2,
-                    "compass": false,
-                });
-            }
-
-            data.markers.forEach(marker => {
-                var markerElement = document.createElement("div");
-                markerElement.id = marker.id;
-                markerElement.className = "marker";
-
-                // Calculate marker coordinates
-                var markerXPercent = (marker.x / miniMapWidth) * 100;
-                var markerYPercent = (marker.y / miniMapHeight) * 100;
-
-                markerElement.style.top = markerYPercent + "%";
-                markerElement.style.left = markerXPercent + "%";
-
-                miniMapContainer.appendChild(markerElement);
-
-                // Add event listener
-                markerElement.addEventListener("click", function() {
-                    pannellum.viewer('panorama', {
-                        "type": "equirectangular",
-                        "panorama": marker.file,
-                        "autoLoad": true,
-                        "showControls": false,
-                        "autoRotate": -2,
-                        "compass": false,
-                    });    
-                });
-            });
-        })
-        .catch(error => console.error("Error loading markers:", error))
+function closeModal() {
+    console.log("dbg");
+    document.getElementById('modal-360').style.display = 'none';
+}
